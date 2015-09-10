374415ed92e47c085470caed39fc34d24fb228ba
lokg <lore9ie@gmail.com>
2015-09-10 19:07:45 +0800

    example update

--- src/main/java/io/mic/TestLogic.java
+++ src/main/java/io/mic/TestLogic.java
@@ -1,6 +1,9 @@
 package io.mic;
 
 import ratpack.server.RatpackServer;
+import ratpack.server.ServerConfig;
+
+import java.net.InetAddress;
 
 /**
  * Created by lwbu on 15-9-10.
@@ -9,6 +12,12 @@ public class TestLogic {
     public static void main(String[] args){
         try {
             RatpackServer.start(server -> server
+                            .serverConfig(
+                                    ServerConfig.embedded()
+                                            .address(InetAddress.getByName("188.166.241.7"))
+                                            .port(8080)
+                                    .threads(2)
+                            )
                             .handlers(chain -> chain
                                             .get(ctx -> ctx.render("Hello World!"))
                                             .get(":name", ctx -> ctx.render("Hello " + ctx.getPathTokens().get("name") + "!"))
